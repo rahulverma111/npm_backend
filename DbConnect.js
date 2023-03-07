3cebafafee106338d065ac8d24574df2a4dffd5c
rahulverma111 <rahul@betalectic.com>
2023-03-07 16:09:58 +0530

    update in online hosting database url

--- DbConnect.js
+++ DbConnect.js
@@ -3,10 +3,13 @@ require('dotenv').config({path:'./config.env'});
 
  ;
 
-const sequelize = new Sequelize(process.env.DATABASE, process.env.USER_NAME, process.env.DATABASE_PASS, {
-    host: 'localhost',
-    dialect:  'postgres' 
-});
+// const sequelize = new Sequelize(process.env.DATABASE, process.env.USER_NAME, process.env.DATABASE_PASS, {
+//     host: 'dpg-cg3gp01mbg5fch4jq5q0-a',
+//     dialect:  'postgres' 
+// });
+
+
+const sequelize = new Sequelize('postgres://rahul:YCLVBNgC6OkqNH0GuyApgYByBhhOCKLQ@dpg-cg3gp01mbg5fch4jq5q0-a.oregon-postgres.render.com/user_database_0xba?ssl=true')
 
 sequelize.authenticate().then(()=>{
     console.log('Connection has been established successfully.');
